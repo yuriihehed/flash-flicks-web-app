bc42fb26fb6a78bcd043bcf43fd20ea518a7239c
Gulbanu Madiyarova <gmadiyar@depaul.edu>
2025-02-19 18:41:44 -0600

    fixing local issues

--- myproject/flashcards/models.py
+++ myproject/flashcards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify 
+
 
 
 # Custom user model
