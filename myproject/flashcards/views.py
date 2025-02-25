fc98db943e734d183e64ceb67b912af9bc54a196
John <johnle4848@gmail.com>
2025-02-25 11:55:29 -0600

    fix login button to redirect to home

--- myproject/flashcards/views.py
+++ myproject/flashcards/views.py
@@ -183,7 +183,7 @@ def login_page(request):
 
         if user:
             login(request, user)
-            return redirect("homepage")  # Redirect to home/dashboard
+            return redirect("home")
         else:
             messages.error(request, "Invalid email or password.")
 
