a08b4cfffbf44a58cb6a1db23117f8e8ecad9e65
Yurii Hehediush <yuriihehed@Yuriis-MacBook-Pro.local>
2025-02-25 22:19:40 -0600

    Resolved

--- myproject/flashcards/urls.py
+++ myproject/flashcards/urls.py
@@ -39,4 +39,8 @@ urlpatterns = [
 
 ]
 
-#if settings.DEBUG:path('lea
+#if settings.DEBUG:path('learning-styles/', views.learning_page, name='learning')
+  #  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
